11c30033f816d3226a7d55e5292359c817e4b2b1
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-10-23 02:33:23 -0400

    added theme toggle

--- docs/js/toggleColor.js
+++ docs/js/toggleColor.js
@@ -3,21 +3,27 @@ window.toggleScheme = function() {
   const preferSupported = window.matchMedia("(prefers-color-scheme)").media !== "not all"
   let scheme = body.getAttribute("data-md-color-scheme")
   let prefer = body.getAttribute("data-md-prefers-color-scheme")
-
-  if (preferSupported && scheme === "default" && prefer !== "true") {
-    prefer = "true"
-    scheme = (window.matchMedia("(prefers-color-scheme: dark)").matches) ? "slate" : "default"
-  } else if (preferSupported && prefer === "true") {
-    prefer = "false"
-    scheme = "slate"
-  } else if (scheme === "slate") {
+  console.log(scheme, prefer)
+  //if (preferSupported && scheme === "default" && prefer !== "true") {
+    //prefer = "true"
+    //scheme = (window.matchMedia("(prefers-color-scheme: dark)").matches) ? "slate" : "default"
+    //console.log("1")
+  //} else if (preferSupported && prefer === "true") {
+    //prefer = "false"
+    //scheme = "slate"
+    //console.log("2")
+  //} else if (scheme === "slate") {
+  if (scheme === "slate") {
     prefer = "false"
     scheme = "default"
+    console.log("3")
   } else {
     prefer = "false"
     scheme = "slate"
+    console.log("4")
   }
   localStorage.setItem("data-md-prefers-color-scheme", prefer)
   body.setAttribute("data-md-prefers-color-scheme", prefer)
   body.setAttribute("data-md-color-scheme", scheme)
 }
+
